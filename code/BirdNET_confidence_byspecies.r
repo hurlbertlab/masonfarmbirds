435589e1840fd73fa005e02c948e4327fcf90072
Hurlbert <hurlbert@AD.UNC.EDU>
2025-08-21 11:20:17 -0400

    violinplots of BirdNET confidence by spp

--- code/BirdNET_confidence_byspecies.r
+++ code/BirdNET_confidence_byspecies.r
@@ -1,7 +1,8 @@
 # Supplemental Figure conveying the distribution of BirdNET confidence scores by species
 
 # Requires dataframe 'shorthand' from PointCountsAnalysis.R
-
+library(vioplot)
+library(dplyr)
 
 compiledConfidence = read.csv('data/CompiledBirdNetResults_2019PointCounts.csv') %>%
   filter(Species %in% shorthand$CommonName,
@@ -20,7 +21,7 @@ compiledConfidence$Species <- factor(compiledConfidence$Species, levels = ordere
 par(mar = c(12, 5, 1, 1))
 
 # Create a vector of colors to assign based on species colors
-viocolors = c(rep('gray50', 5), "blue", 'gray50', 'gray50', "red", rep('gray50', 8))
+viocolors = c(rep('gray50', 5), "#FF7700", 'gray50', 'gray50', "#4488D2", 'gray50', "#F0B924", "#FD569C", 'gray50', "#0ACE79", rep('gray50', 6), "#D58DFF", rep('gray50', 5))
 
 vioplot(Confidence ~ Species, data = compiledConfidence,
         ylab = "BirdNET Confidence", xaxt = "n", xlab = "", col = viocolors)
